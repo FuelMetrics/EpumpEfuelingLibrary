32123404e0fe1855180434702e12d490557addf0
Christian Olatomiwa <softwareolatomiwa@gmail.com>
2021-10-23 22:38:58 +0100

    Update include; -Connection optimization -Library error update

---
+++ EfuelingLibrary/build/generated/source/buildConfig/debug/ng/com/epump/efueling/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ng.com.epump.efueling;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "ng.com.epump.efueling";
+  public static final String BUILD_TYPE = "debug";
+}
